efdc435354fbc48f717e4a527405cea2ed20e81c
Snehangshu Bhattacharya <snehangshub1.618@gmail.com>
2024-02-23 00:25:22 +0530

    Added plaintext support

--- src/main/java/dev/snehangshu/tldr/services/FileHandlerService.java
+++ src/main/java/dev/snehangshu/tldr/services/FileHandlerService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @Service
 public class FileHandlerService {
@@ -18,7 +20,7 @@ public class FileHandlerService {
     private GeminiRestService geminiRestService;
 
 
-    public String processFile(MultipartFile doc, float temperature, int maxTokens) {
+    public String processFile(MultipartFile doc, float temperature, int maxTokens) throws IOException {
         if (doc.isEmpty()) {
             throw new RuntimeException("Looks like your file attachment skills are taking a coffee break! Better luck next upload!");
         }
@@ -26,7 +28,13 @@ public class FileHandlerService {
         if (!StringUtils.equalsAny(contentType, "application/pdf", "text/plain")) {
             throw new RuntimeException("Only text and pdf files are allowed!");
         }
-        String extractedText = pdfExtractorService.extractText(doc);
+        String extractedText = "";
+        if (Objects.equals(contentType, "application/pdf")) {
+            extractedText = pdfExtractorService.extractText(doc);
+        } else if (Objects.equals(contentType, "text/plain")) {
+            extractedText = new String(doc.getBytes(), StandardCharsets.UTF_8);
+        }
+
         GeminiResponse chatResponse = geminiRestService.chat(extractedText, temperature, maxTokens);
 
         return chatResponse.getCandidates().getFirst().getContent().getParts().getFirst().getText().trim();
